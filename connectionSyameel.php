9ee6b8761f3041bde1446998879e4338485e05cf
syameelamni5-blip <syameelamni5@gmail.com>
2025-12-10 12:45:13 +0800

    connect

---
+++ connectionSyameel.php
@@ -0,0 +1,15 @@
+<?php
+$host = "localhost";
+$port = "5432";
+$dbname = "Workshop";
+$user = "postgres";
+$password = "admin";
+
+try {
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} 
+catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
