800ac374257767c95170dd374de2934f66717e76
epanadiaa <epanadia@gmail.com>
2025-12-23 23:13:17 +0800

    audit

---
+++ audit.php
@@ -0,0 +1,34 @@
+<?php
+function logAudit(
+    $conn,
+    $action,
+    $module,
+    $description
+) {
+    if (!isset($_SESSION['user_id'])) {
+        return;
+    }
+
+    $userId   = $_SESSION['user_id'];
+    $username = $_SESSION['username'];
+    $role     = $_SESSION['role'];
+    $ip       = $_SERVER['REMOTE_ADDR'];
+
+    $sql = "
+        INSERT INTO audit_trail
+        (user_id, username, role, action, module, description, ip_address)
+        VALUES
+        (:user_id, :username, :role, :action, :module, :description, :ip)
+    ";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->execute([
+        ':user_id' => $userId,
+        ':username' => $username,
+        ':role' => $role,
+        ':action' => $action,
+        ':module' => $module,
+        ':description' => $description,
+        ':ip' => $ip
+    ]);
+}
